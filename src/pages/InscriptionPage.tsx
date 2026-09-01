c43bb6163ba3137bb4df77e7b01a34b84c6c433c
activsportrivesaltes <activsportrivesaltes@etik.com>
2026-09-01 19:48:58 +0200

    Modifs

--- src/pages/InscriptionPage.tsx
+++ src/pages/InscriptionPage.tsx
@@ -48,7 +48,7 @@ export default function InscriptionPage() {
         subtitle={
     <>
       Pour vous inscrire, plus rapidement, apportez la fiche d'inscription et le formulaire de santé que vous pouvez télécharger ci-dessous.<br />Prévoyez 3 enveloppes timbrées à votre adresse si vous n'avez pas d'adresse mail.<br />
-      Une question ? N'hésitez pas à nous envoyer un message, nous reviendrons vers vous rapidement.<br />Les inscriptions  auront lieu les mercredi 9 et jeudi 10 septembre de 15h à 19h à l'Ami&#8209;Club.
+      Une question ? N'hésitez pas à nous envoyer un message, nous reviendrons vers vous rapidement.<br />Les inscriptions  auront lieu les mercredi 9 et jeudi 10 septembre de 15h à 19h à l'Ami-Club.
     </>
   }
   
