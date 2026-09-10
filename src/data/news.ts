ab677b6bae6c8f169a3bbdc79c705aa49f2246b1
activsportrivesaltes <activsportrivesaltes@etik.com>
2026-09-10 11:31:48 +0200

    MAJ

--- src/data/news.ts
+++ src/data/news.ts
@@ -18,9 +18,9 @@ export const news: NewsItem[] = [
     date: '2026-09-03',
     category: 'Info club',
     excerpt:
-      'Notre assemblée générale annuelle s\'est tenue le jeudi 3 septembre 2026. L\'assemblée a validée le changement de nom de l\'association : Activ\' Sport Rivesaltes.',
+      'Notre assemblée générale annuelle s\'est tenue le jeudi 3 septembre 2026. L\'assemblée a validé le changement de nom de l\'association : Activ\' Sport Rivesaltes.',
     content:
-      'L\'assemblée générale d\'Activ\' Sport Rivesaltes se tiendra le jeudi 3 septembre 2026 à 17h30 à l\'Ami-Club. Au programme : Changement de nom de l\'association,bilan moral et financier de la saison écoulée, élection du bureau pour la nouvelle saison, présentation des activités et du planning 2026-2027. Votre présence est importante pour la vie démocratique du club.',
+      'L\'assemblée générale d\'Activ\' Sport Rivesaltes s\'est tenue le jeudi 3 septembre 2026. L\'assemblée a validé le changement de nom de l\'association : Activ\' Sport Rivesaltes.',
     image: '/images/AG2026F.jpg',
   },
   
